7f403f9ea20f26fc9761bc57ccce0ec804119957
Robert Smart <roca1388@gmail.com>
2021-12-10 21:02:55 -0300

    Deployment

--- Citas-React/tailwind.config.js
+++ Citas-React/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{jsx}"],
+  purge: ["index.html", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
